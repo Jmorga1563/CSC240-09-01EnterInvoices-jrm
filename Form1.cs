f90c4ab3022bf7aa940b5babd18f6126e509a26a
jmorga1563 <jmorga1563@my.sullivan.edu>
2024-02-26 10:28:49 -0500

    Enter button click method.

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,16 @@ namespace CSC240_09_01EnterInvoices_jrm
         {
             InitializeComponent();
         }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            num = Convert.ToInt32(invoiceBox.Text);
+            name = nameBox.Text;
+            amount = Convert.ToDouble(amountBox.Text);
+            writer.WriteLine(num + DELIM + name + DELIM + amount);
+            invoiceBox.Clear();
+            nameBox.Clear();
+            amountBox.Clear();
+        }
     }
 }
