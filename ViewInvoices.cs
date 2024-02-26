345584ff0417545597943581686bfa925c6228bb
jmorga1563 <jmorga1563@my.sullivan.edu>
2024-02-26 11:19:35 -0500

    Added catch for null reference, closed writer when opening view form so it works in one program.

--- ViewInvoices.cs
+++ ViewInvoices.cs
@@ -26,11 +26,20 @@ namespace CSC240_09_01EnterInvoices_jrm
 
         private void ViewButton_Click(object sender, EventArgs e)
         {
-            recordIn = reader.ReadLine();
-            fields = recordIn.Split(DELIM);
-            vInvoiceBox.Text = fields[0];
-            vNameBox.Text = fields[1];
-            vAmountBox.Text = fields[2];
+            try
+            {
+                StreamReader reader = new StreamReader(file);
+                recordIn = reader.ReadLine();
+                fields = recordIn.Split(DELIM);
+                vInvoiceBox.Text = fields[0];
+                vNameBox.Text = fields[1];
+                vAmountBox.Text = fields[2];
+            }
+            catch (NullReferenceException)
+            {
+                viewPromptLabel.Text = "You have viewed \nall the records.";
+                viewButton.Enabled = false;
+            }
         }
     }
 }
